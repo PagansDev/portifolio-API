5f9ae7d62876dd49e28f1811ed0124260376d088
AlienUrbano <paulogabrielneves@hotmail.com>
2025-04-19 20:24:04 -0300

    PROD-v1

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+let sequelize;
+
+// Verifica se está em produção
+if (process.env.NODE_ENV === 'production') {
+  // Usa a URL de conexão do Railway
+  sequelize = new Sequelize(process.env.MYSQL_URL || process.env.DATABASE_URL, {
     dialect: 'mysql',
     logging: false,
     define: {
@@ -15,7 +14,30 @@ const sequelize = new Sequelize(
       underscored: false,
       freezeTableName: true,
     },
-  }
-);
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+  });
+} else {
+  // Configuração para desenvolvimento local
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'mysql',
+      logging: false,
+      define: {
+        timestamps: true,
+        underscored: false,
+        freezeTableName: true,
+      },
+    }
+  );
+}
 
 module.exports = sequelize;
